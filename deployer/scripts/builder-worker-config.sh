c986598b64da09be4be0a6c6850a77896a3d56d0
Kristiyana Gacheva <kristiyana.gacheva@gmail.com>
2021-06-26 17:33:12 +0300

    Deployer: Extend the script for configuring the BuilderWorker

--- deployer/scripts/builder-worker-config.sh
+++ deployer/scripts/builder-worker-config.sh
@@ -1,2 +1,42 @@
 #!/bin/sh
-yum install -y qemu-kvm
+
+# Set SELinux to permissive and stop the firewall
+setenforce 0
+systemctl stop firewalld
+
+# Install packer
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum install -y packer
+
+# Install supplementary packages
+yum -y install epel-release
+yum install -y qemu-kvm htop screen dnf
+
+# Copy the qemu-kvm exec to the name expected by Packer
+cp /usr/libexec/qemu-kvm /usr/libexec/qemu-system-x86_64
+
+# Install azcopy
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -zxvf downloadazcopy-v10-linux
+cp azcopy_linux_amd64_10.11.0/azcopy /usr/libexec
+rm -r downloadazcopy-v10-linux azcopy_linux_amd64_10.11.0
+
+# Install az
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat << EOF > /etc/yum.repos.d/azure-cli.repo
+[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc"
+EOF
+dnf install -y azure-cli
+
+# Add /usr/libexec to the PATH
+echo 'export PATH=$PATH:/usr/libexec' >> ~/.bashrc
+source ~/.bashrc
+
+# Add packer_admin to the kvm group
+usermod -aG kvm packer_admin
