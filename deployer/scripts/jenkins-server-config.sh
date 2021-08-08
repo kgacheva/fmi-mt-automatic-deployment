f741299c0168b55a3f86d65b9cfce140d4c06174
Kristiyana Gacheva <kristiyana.gacheva@gmail.com>
2021-08-08 19:29:49 +0300

    Deployer: Install Terraform on Jenkins server

--- deployer/scripts/jenkins-server-config.sh
+++ deployer/scripts/jenkins-server-config.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# Install OpenJDK and Jenkins
 apt install openjdk-8-jdk -y
 wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update && apt-get install jenkins -y
+
+# Install Terraform
+apt install unzip -y
+wget https://releases.hashicorp.com/terraform/1.0.4/terraform_1.0.4_linux_amd64.zip
+unzip terraform_1.0.4_linux_amd64.zip
+chown jenkins_admin:jenkins_admin terraform
+mv terraform /usr/local/bin/
+
 service jenkins restart
