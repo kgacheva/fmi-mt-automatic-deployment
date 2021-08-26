1adb29342e934734bcf18176451157f338070e82
Kristiyana Gacheva <kristiyana.gacheva@gmail.com>
2021-08-26 16:43:42 +0300

    Deployer: Clean up installation of az on builder-worker

--- deployer/scripts/builder-worker-config.sh
+++ deployer/scripts/builder-worker-config.sh
@@ -16,25 +16,6 @@ yum install -y qemu-kvm htop screen dnf git
 # Copy the qemu-kvm exec to the name expected by Packer
 cp /usr/libexec/qemu-kvm /usr/libexec/qemu-system-x86_64
 
-# Install azcopy
-wget https://aka.ms/downloadazcopy-v10-linux
-tar -zxvf downloadazcopy-v10-linux
-cp azcopy_linux_amd64_10.11.0/azcopy /usr/libexec
-rm -r downloadazcopy-v10-linux azcopy_linux_amd64_10.11.0
-sudo chown packer_admin:packer_admin /usr/libexec/azcopy
-
-# Install az
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-cat << EOF > /etc/yum.repos.d/azure-cli.repo
-[azure-cli]
-name=Azure CLI
-baseurl=https://packages.microsoft.com/yumrepos/azure-cli
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc"
-EOF
-dnf install -y azure-cli
-
 # Add /usr/libexec to the PATH of the packer_admin user
 echo 'export PATH=$PATH:/usr/libexec' >> /home/packer_admin/.bashrc
 
